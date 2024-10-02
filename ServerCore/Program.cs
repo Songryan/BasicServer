44e1aa31a2748098b1a9830bafff37dad0e3480e
RyanSong <tjr6255@gmail.com>
2024-10-02 17:56:53 +0900

    [Test] Thread 테스트.

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class ServerCore
+{
+    static void MainThread()
+    {
+        while (true)
+            Console.WriteLine("Hello Thread!");
+    }
+
+    static void Main(string[] args)
+    {
+        Thread t = new Thread(MainThread);
+        t.IsBackground = true;
+        t.Start();
+        Console.WriteLine("Hello World!");
+    }
+}
