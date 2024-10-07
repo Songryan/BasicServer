c58f2a2dfd3ffac1471fab074669d0e5de1a61f2
RyanSong <tjr6255@gmail.com>
2024-10-07 17:52:05 +0900

    [Tread] Test

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,18 +1,66 @@
 ﻿using System;
 
-class ServerCore
+namespace ServerCore
 {
-    static void MainThread()
+    class SpintLock
     {
-        while (true)
-            Console.WriteLine("Hello Thread!");
+        volatile int _locked = 0;
+
+        public void Acquire()
+        {
+            while (true)
+            {
+                int expected = 0;
+                int desired = 1;
+                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
+                    break;
+
+                // 쉬다 올게~
+
+            }
+        }
+
+        public void Release()
+        {
+            _locked = 0;
+        }
     }
 
-    static void Main(string[] args)
+    class Program
     {
-        Thread t = new Thread(MainThread);
-        t.IsBackground = true;
-        t.Start();
-        Console.WriteLine("Hello World!");
+        static int _num = 0;
+        static SpintLock _lock = new SpintLock();
+
+        static void Thread1()
+        {
+            for(int i = 0; i < 100000; i++)
+            {
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
+            }
+        }
+
+        static void Thread2()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread1);
+            Task t2 = new Task(Thread2);
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(_num);
+        }
     }
 }
