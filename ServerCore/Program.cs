a8b29dad7918822d14e252b51d844701c3b562fd
RyanSong <tjr6255@gmail.com>
2024-10-08 17:00:39 +0900

    [Test] Free-Lock (Reader / Writer Lock)

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,65 +2,38 @@
 
 namespace ServerCore
 {
-    class SpintLock
-    {
-        volatile int _locked = 0;
-
-        public void Acquire()
-        {
-            while (true)
-            {
-                int expected = 0;
-                int desired = 1;
-                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
-                    break;
-
-                // 쉬다 올게~
-
-            }
-        }
-
-        public void Release()
-        {
-            _locked = 0;
-        }
-    }
-
     class Program
     {
-        static int _num = 0;
-        static SpintLock _lock = new SpintLock();
-
-        static void Thread1()
-        {
-            for(int i = 0; i < 100000; i++)
-            {
-                _lock.Acquire();
-                _num++;
-                _lock.Release();
-            }
-        }
-
-        static void Thread2()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.Acquire();
-                _num--;
-                _lock.Release();
-            }
-        }
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread1);
-            Task t2 = new Task(Thread2);
+            Task t1 = new Task(delegate ()
+            {
+                for(int i =0; i < 10000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t2 = new Task(delegate ()
+            {
+                for(int i =0; i < 10000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
+
             t1.Start();
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(_num);
+            Console.WriteLine(count);
         }
     }
 }
